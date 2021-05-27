7cf285f84c2bdce8a9809b071a6cf75a5b5422d9
Imtiaz Miah <imtiazmiah@gmail.com>
2021-05-27 18:09:33 +0000

    Add categrory controller functions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,10 +1,31 @@
+#based on products controller
+
 class Admin::CategoriesController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @categories = Category.new
   end
 
   def create
+    @categories = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
