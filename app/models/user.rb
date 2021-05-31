dd5a0eb07a2d10777563bfa78fd840d2ba37aef7
Imtiaz Miah <imtiazmiah@gmail.com>
2021-05-31 23:50:23 +0000

    Add more validation tests and update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
     it "should create a new user with all the fields filled in" do
       @user = User.new(
         name: "Rick",
-        email: "rick@dsancez.ca",
+        email: "rick@sanchez.ca",
         password: "wubbalubbadubdub",
         password_confirmation: "wubbalubbadubdub"
       )
@@ -17,9 +17,58 @@ RSpec.describe User, type: :model do
     it "should not save user if password is missing" do
       @user = User.new(
         name: "Rick",
-        email: "rick@dsancez.ca",
+        email: "rick@sanchez.ca",
         password: nil,
         password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it "should not save user if confirmation password is wrong" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@sanchez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "derp"
+      )
+      @user.save
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it "should not save user if name is missing" do
+      @user = User.new(
+        name: nil,
+        email: "rick@sanchez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+
+    it "should not save user if email is missing" do
+      @user = User.new(
+        name: "Rick",
+        email: nil,
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it "should not save if password is less than 6 characters" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@sanchez.ca",
+        password: "derp",
+        password_confirmation: "derp"
       )
       @user.save
       expect(@user).to be_invalid
