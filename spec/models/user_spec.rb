72fa59924e36c089a59451ba9c408a45f8943939
Imtiaz Miah <imtiazmiah@gmail.com>
2021-05-31 21:38:11 +0000

    Add valid user register test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+
+    it "creates a new user with all the fields filled in" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@dsancez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      expect(@user).to be_valid
+    end
+
+
+
+  end
 end
