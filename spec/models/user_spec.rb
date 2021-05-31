a852088bb17af6875b3185cc8e5d59f6d04d5991
Imtiaz Miah <imtiazmiah@gmail.com>
2021-05-31 22:29:09 +0000

    Add password validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,30 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validation' do
 
-    it "creates a new user with all the fields filled in" do
+    it "should create a new user with all the fields filled in" do
       @user = User.new(
         name: "Rick",
         email: "rick@dsancez.ca",
         password: "wubbalubbadubdub",
         password_confirmation: "wubbalubbadubdub"
       )
+      @user.save
       expect(@user).to be_valid
     end
 
+    it "should not save user if password is missing" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@dsancez.ca",
+        password: nil,
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to_not be_empty
+    end
 
 
   end
+
 end
