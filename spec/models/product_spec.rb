6bbd14b3f4b7b0e57514b61c5b7b97bf7fe7cc27
Imtiaz Miah <imtiazmiah@gmail.com>
2021-05-31 18:19:35 +0000

    Add passing test for all fields filled correctly

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+  it "creates category and product with all 4 fileds filled" do
+    @category = Category.new(name: "Rick Sanchez' Inventions")
+    @category.save
+    @product = Product.new(
+      name: "Portal gun", 
+      price: 666, 
+      quantity: 9, 
+      category: @category
+    )
+    expect(@product).to be_valid
+  end
+
 end
