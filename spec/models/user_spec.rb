8c64f1b5180d20eaf23043a374f549906bc02067
Imtiaz Miah <imtiazmiah@gmail.com>
2021-06-01 03:32:59 +0000

    Add authenticate_with_credentials tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,8 +74,72 @@ RSpec.describe User, type: :model do
       expect(@user).to be_invalid
       expect(@user.errors.full_messages).to_not be_empty
     end
+  end
 
+  ############### CREDENTIAL AUTHENTICATIONS #################
 
+  describe '.authenticate_with_credentials' do
+    it "should return true when email and password match" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@sanchez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      @test = User.authenticate_with_credentials("rick@sanchez.ca", "wubbalubbadubdub")
+      expect(@test).to be_truthy
+    end
+
+    it "should fail if the password is wrong" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@sanchez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      @test = User.authenticate_with_credentials("rick@sanchez.ca", "herpderp")
+      expect(@test).to be_falsey
+    end
+
+    it "should fail if the email is wrong" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@sanchez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      @test = User.authenticate_with_credentials("rick@pickle.ca", "wubbalubbadubdub")
+      expect(@test).to be_falsey
+    end
+
+    it "should return true when email is of different cases" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@sanchez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      @test = User.authenticate_with_credentials("rIck@saNCHez.ca", "wubbalubbadubdub")
+      expect(@test).to be_truthy
+    end
+
+    it "should return true when email has leading or ending spaces" do
+      @user = User.new(
+        name: "Rick",
+        email: "rick@sanchez.ca",
+        password: "wubbalubbadubdub",
+        password_confirmation: "wubbalubbadubdub"
+      )
+      @user.save
+      @test = User.authenticate_with_credentials("  rIck@saNCHez.ca   ", "wubbalubbadubdub")
+      expect(@test).to be_truthy
+    end
+
+  
   end
 
 end
